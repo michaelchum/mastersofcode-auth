cabfb7875edecc39f2cef1d18780a7de3b2569c2
Michael <michaelhochum@gmail.com>
2015-08-22 19:53:11 -0700

    Initial Commit

---
+++ WebSocketAndDigitsAuth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebSocketAndDigitsAuth
+//
+//  Created by Michael Ho on 2015-08-22.
+//  Copyright (c) 2015 michaelchum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
